11bedaa68fa7bcd60d95c6045aa236aaf70ef86d
GabrielVCastro <gabriel.gvc2020@gmail.com>
2018-08-22 07:48:47 -0300

    Adicionando tabelas e corrigindo erro ao deletar os chamados em outros menores

--- chamados/exibir_lista.php
+++ chamados/exibir_lista.php
@@ -5,8 +5,23 @@ include("../classes/Chamado.class.php");
 $chamado = new Chamado();
 $lista =$chamado->exibir_lista();
 
+?>
 
+<div class="container">
+	<div class="row	">
+		<div class="offset-xl-2 col-xl-2 offset-lg-1 col-lg-2 offset-md-1 col-md-2 offset-2 col-2">
+			<br>
+			<a href="form_cadastrar_chamados.php" class="btn btn-info cadastrar">Cadastrar</a>
+			
+		</div>
+		<div class="offset-xl-5 col-xl-2 offset-lg-7 col-lg-2 offset-md-7 col-md-2">
+			<br>
+			<a href="../index.php" class="btn btn-warning">Voltar</a>
 
+		</div>
+	</div>
+
+<?php
 if((isset($lista) && (count($lista)>0))){ ?>
 		<div class="container">
 			<br>
@@ -30,17 +45,7 @@ if((isset($lista) && (count($lista)>0))){ ?>
 			<?php }
 				$_SESSION['erro'] = null;
 			?>
-			<div class="row	">
-					<div class="offset-xl-2 col-xl-2 offset-lg-1 col-lg-2 offset-md-1 col-md-2 offset-2 col-2">
-					<br>
-					<a href="form_cadastrar_chamados.php" class="btn btn-info cadastrar">Cadastrar</a>
-					
-				</div>
-				<div class="offset-xl-5 col-xl-2 offset-lg-7 col-lg-2 offset-md-7 col-md-2">
-					<br>
-					<a href="../index.php" class="btn btn-warning">Voltar</a>
-
-				</div>
+			
 			</div><br>
 			<div class="row">
 				<h2>Lista de chamados</h2>
@@ -102,6 +107,14 @@ if((isset($lista) && (count($lista)>0))){ ?>
 				</table>
 			</div>
 		</div>
+<?php }else{ ?>
+			<div class="container">
+
+				<br><br>
+				<div class="alert alert-warning" role="alert">
+				 	<p><strong>Nenhum chamado feito!  feito!</strong></p>
+				</div>
+			</div>
 <?php }
 ?>
 
